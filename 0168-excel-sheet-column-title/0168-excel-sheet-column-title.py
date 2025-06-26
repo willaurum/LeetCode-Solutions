72f0c4730c1a1be0f7cfd8de88a1900d27f1de2c
William Cook <williamcook0811@gmail.com>
2025-06-26 16:12:08 -0400

    Time: 0 ms (100%), Space: 17.7 MB (61.52%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def convertToTitle(self, columnNumber):
+        ans = ""
+        while columnNumber > 0:
+            ans = chr(ord('A') + (columnNumber - 1) % 26) + ans
+            columnNumber = (columnNumber - 1) // 26
+        return ans
